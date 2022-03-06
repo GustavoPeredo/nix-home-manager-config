a9576f28753bcc9361be4c5c7e626b293f81c6d0
GustavoPeredo <gustavomperedo@protonmail.com>
2022-03-06 21:57:22 +0100

    Inital commit

---
+++ config/konsole.profile
@@ -0,0 +1,13 @@
+[Appearance]
+ColorScheme=konsole
+
+[General]
+Name=Profile 1
+Parent=FALLBACK/
+
+[Interaction Options]
+TextEditorCmd=1
+UnderlineFilesEnabled=true
+
+[Keyboard]
+KeyBindings=default
---
+++ home.nix
@@ -0,0 +1,122 @@
+{ config, pkgs, ... }:
+{
+  # Custom neovim config
+  nixpkgs.overlays = [ 
+    (self: super: {
+    neovim = super.neovim.override {
+      viAlias = true;
+      vimAlias = true;
+      configure = {
+        packages.myVimPackage = with pkgs.vimPlugins; {
+          start = [ vimtex goyo-vim vim-nix coc-nvim coc-css coc-json coc-tsserver coc-html coc-vimtex coc-python coc-clangd ];
+        };
+	customRC = builtins.readFile ./config/nvim.conf;
+      };
+    };
+    }) 
+  ];
+
+  # Packages
+  home.packages = with pkgs; [
+    # Shell
+    bash
+
+    # Apps
+    todo-txt-cli
+    neovim
+    yank
+    ranger
+    # texlive.combined.scheme-full
+
+    # coc workaround
+    nodejs
+  ];
+
+  # Options
+  programs = {
+    git = {
+      enable = true;
+      userName = "GustavoPeredo";
+      userEmail = "gustavomperedo@protonmail.com";
+    };
+    bash = {
+      enable = true;
+      bashrcExtra = builtins.readFile ./config/bashrc;
+      historyIgnore = [ "ls" "cd" "exit" "history" ];
+    };
+  };
+
+  # Automatically add tasks to todo
+  systemd.user = {
+    services = {
+      daily_tasks = {
+        Unit = {
+          Description = "Add daily tasks script";
+        };
+        Service = {
+          ExecStart = ''
+            ${pkgs.bash}/bin/bash -c '\
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg archive ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(A) Do exercise @$(date +%%a) +Lifestyle" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(A) Practice Anki @$(date +%%a) +Uni" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(B) Organize myself @$(date +%%a) +Lifestyle" '
+        '';
+        };
+      };
+      weekly_tasks = {
+        Unit = {
+          Description = "Add weekly tasks script";
+        };
+        Service = {
+          ExecStart = ''
+            ${pkgs.bash}/bin/bash -c '\
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Study mathematics @Mon +Uni" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Study/add cards Statistik @Tue +Uni" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Study/add cards BWL @Thu +Uni" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Study Info @Fri +Uni" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Study/add cards FOC @Thu +Uni" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(D) Plan video and write script @Mon +Channel" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(D) Record videos @Tue +Channel" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Edit video and publish it @Wed +Channel" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(D) Livestream @Fri +Channel" ; \
+\
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(D) Plan what to do on Saturday @Thu +Lifestyle" ; \
+            ${pkgs.todo-txt-cli}/bin/todo.sh -d ${config.home.homeDirectory}/.local/share/todo/todo.cfg add "(C) Adventure time! @Sat +Lifestyle" '
+        '';
+        };
+      };
+    };
+    timers = {
+      daily_tasks = {
+        Unit = {
+          Description = "Add daily tasks timer";
+        };
+        Timer = {
+          OnCalendar = "daily";
+          Persistent = true;
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
+        };
+      };
+      weekly_tasks = {
+        Unit = {
+          Description = "Add weekly tasks timer";
+        };
+        Timer = {
+          OnCalendar = "weekly";
+          Persistent = true;
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
+        };
+      };
+    };
+  };
+
+  # Write config files
+  home.file = {
+    "${config.home.homeDirectory}/.local/share/konsole/konsole.colorscheme".source = ./config/konsole.colorscheme;
+    "${config.home.homeDirectory}/.local/share/konsole/Profile 1.profile".source = ./config/konsole.profile;
+  };
+}
